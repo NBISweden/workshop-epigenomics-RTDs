e2b1a9d2eb278154b7529a7f90cb52bfdfa4afaa
agata-sm <smialowskaa@gmail.com>
2021-10-18 10:18:59 +0200

    remove _static from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static','slides']
+html_static_path = ['slides']
 
 #https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
 master_doc = 'index'
